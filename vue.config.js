8c7d9a6fda4d87e6d13dcc0e977c3d998db478f9
Shinsuke OTA <ota@rcnp.s.u-tokyo.ac.jp>
2026-06-27 15:26:16 +0900

    server ip

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
+  devServer : {
     port: 8081,
-    host: 'localhost'
+    host: '0.0.0.0'
   }
 
 })
